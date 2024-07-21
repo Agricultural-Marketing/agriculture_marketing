a52c524991dc08ba15b96c35ac21b11dbceb5bd7
Muhammad Salama <mohamed.salama@42.guru>
2024-07-21 22:57:40 +0300

    add grand total field and update its value

--- agricultural_marketing/agricultural_marketing/doctype/invoice_form/invoice_form.py
+++ agricultural_marketing/agricultural_marketing/doctype/invoice_form/invoice_form.py
@@ -6,4 +6,10 @@ from frappe.model.document import Document
 
 
 class InvoiceForm(Document):
-	pass
+	def on_update(self):
+		self.update_grand_total()
+
+	def update_grand_total(self):
+		self.grand_total = 0
+		for item in self.items:
+			self.grand_total += item.total
